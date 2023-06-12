44ec4801535cfec58a28340c6e41c1a99c37561e
litunkhan <litunkhan6@gmail.com>
2023-06-12 19:48:40 +0600

    redirect-file-added

--- src/Pages/Firebase/Firebase.config.jsx
+++ src/Pages/Firebase/Firebase.config.jsx
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjLoHKvoMtkevaCuazlBPwvqjiKNVZMRw",
-  authDomain: "summer-camp-17246.firebaseapp.com",
-  projectId: "summer-camp-17246",
-  storageBucket: "summer-camp-17246.appspot.com",
-  messagingSenderId: "547695589370",
-  appId: "1:547695589370:web:afeefb3002c646472d1643"
+  apiKey: import.meta.env.VITE_apiKey,
+  
+  authDomain: import.meta.env.VITE_authDomain,
+  
+  projectId: import.meta.env.VITE_projectId,
+  
+  storageBucket: import.meta.env.VITE_storageBucket,
+  
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  
+  appId: import.meta.env.VITE_appId
+ 
 };
 
 // Initialize Firebase
